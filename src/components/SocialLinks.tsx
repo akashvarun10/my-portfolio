4d497c57076145448a9b1d391500eb1c8d9e2f6e
Pemmaraju Venkat Akash Varun <pemmaraju.v@northeastern.edu>
2024-06-14 11:41:02 +0530

    removed files that are not required

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,18 +1,28 @@
 import { Button, Box, Tooltip } from '@mui/material';
 import { FaLinkedin, FaGithub, FaMedium, FaFileAlt } from 'react-icons/fa';
+import { useContext } from 'react';
+import { ThemeContext } from '../theme/ThemeContext';
 
 const SocialLinks = () => {
+  const themeContext = useContext(ThemeContext);
+
+  if (!themeContext) {
+    throw new Error('SocialLinks must be used within a ThemeProvider');
+  }
+
+  const { mode } = themeContext;
+
   const iconStyle = {
-    backgroundColor: 'white',
-    color: '#76885B',
+    backgroundColor: mode === 'dark' ? '#333' : 'white',
+    color: mode === 'dark' ? 'white' : '#76885B',
     borderRadius: '50%',
     padding: '10px',
     minWidth: 'unset',
     margin: '0 10px', // Adds space between icons
     transition: 'background-color 0.3s, color 0.3s', // Add transition effect
     '&:hover': {
-      backgroundColor: '#76885B',
-      color: 'white',
+      backgroundColor: mode === 'dark' ? 'white' : '#76885B',
+      color: mode === 'dark' ? '#333' : 'white',
       transform: 'scale(1.2)', // Add scale effect on hover
       zIndex: 2, // Ensure the hovered icon is on top
     },
