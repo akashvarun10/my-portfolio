c8e9f9694e67e542e5a8ae809b4e977dfb3a257e
Pemmaraju Venkat Akash Varun <pemmaraju.v@northeastern.edu>
2024-08-08 18:34:06 +0530

    added nginx conf and dockerized

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,20 @@
-# Use a Node image to build the React app
-FROM node:18 AS build
+# Use an official Node.js LTS (Long Term Support) image as a base
+FROM node:lts-alpine
 
+# Set the working directory inside the container
 WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install 
+
+# Copy the rest of the project files to the working directory
 COPY . .
-RUN npm install
-RUN npm run build
 
-# Use the official Nginx image to serve the built files
-FROM nginx:alpine
+# Expose the port your app runs on 
+EXPOSE 5173
 
-# Copy the build files from the build stage to the Nginx html directory
-COPY --from=build /app/dist /usr/share/nginx/html
-
-# Copy the custom Nginx configuration file
-COPY nginx.conf /etc/nginx/nginx.conf
-
-# Expose port 80
-EXPOSE 80
-
-# Start Nginx
-CMD ["nginx", "-g", "daemon off;"]
+# Run your app in development mode
+CMD ["npm", "run", "dev"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import AddProject from './pages/AddProject';
 
-// Initialize Google Analytics
-ReactGA.initialize(import.meta.env.VITE_ANALYTICS_ID); 
-
-const TrackPageView: React.FC = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
-  return null;
-};
 
 const App: React.FC = () => {
   return (
     <Router>
-      <TrackPageView />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/addproject" element={<AddProject />} />
@@ -31,3 +17,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+
